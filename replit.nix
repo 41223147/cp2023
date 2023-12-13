0c98687f09e108d15917642eb60d8efb24c70f47
41223147 <144767826+41223147@users.noreply.github.com>
2023-12-13 12:18:19 +0000

    新增內容

--- replit.nix
+++ replit.nix
@@ -1,5 +1,14 @@
 { pkgs }: {
   deps = [
+    pkgs.libev
     pkgs.llvmPackages_rocm.clang
+    pkgs.gnuplot
+    pkgs.ncurses.dev
+    pkgs.gd
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libev
+    ];
+  };
 }
