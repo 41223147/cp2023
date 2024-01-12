d28e176ae8b15bdf8964580f9287b561d262ac0d
41223147 <144767826+41223147@users.noreply.github.com>
2024-01-12 07:18:05 +0000

    更改ANSIC的分支頁標題

--- downloads/W6.c
+++ downloads/W6.c
@@ -1,15 +1,14 @@
-#include <stdio.h>     // Include the standard input/output header file.
+int main()  
+{
+    // Print Name
+    printf("Name   :TSAI-FU，JING\n"); 
 
-int num1, num2, num3;   /* declaration of three variables */  
-int sum;                /* variable to store the sum of numbers */
-char line_text[50];     /* line of input from keyboard */ 
+    // Print Date of Birth
+    printf("DOB    : July 14, 2004\n"); 
 
-int main() 
-{  
-printf("Input three numbers separated by comma : ");   // Prompt the user to input three numbers separated by comma.
-fgets(line_text, sizeof(line_text), stdin);   // Read a line of input from the user and store it in 'line_text'.
-sscanf(line_text, "10, 15, 10", &num1, &num2, &num3);   // Convert the input to integers and store them in 'num1', 'num2', 'num3'.
-sum = num1 + num2 + num3;   // Calculate the sum of the three numbers.
-printf("The sum of three numbers : %d\n", sum);   // Print the sum of the three numbers.  
-return(0);   // Return 0 to indicate successful execution of the program.  
-}  
+    // Print Mobile Number
+    printf("Mobile : 0922-444-666\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
