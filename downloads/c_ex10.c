1e6820b19cb7c2d121562827d020777e36b89548
41223147 <144767826+41223147@users.noreply.github.com>
2024-01-12 11:57:40 +0000

    新增ex7~ex10

---
+++ downloads/c_ex10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* read and display a number */
+int main () {
+ char c;
+ printf("Mechanicl design: ");
+ c = getchar(); /* read the character in */
+ printf("Design mechanicl: ");
+ putchar(c); /* write the character */
+ return(0);
+}
